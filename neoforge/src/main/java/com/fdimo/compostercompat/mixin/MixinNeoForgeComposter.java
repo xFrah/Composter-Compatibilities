d72aa02903de93b6b739c691ac0a9125b50e7efb
xFrah <fdimonaco309@hotmail.com>
2026-08-08 21:32:04 +0200

    Added tags

--- neoforge/src/main/java/com/fdimo/compostercompat/mixin/MixinNeoForgeComposter.java
+++ neoforge/src/main/java/com/fdimo/compostercompat/mixin/MixinNeoForgeComposter.java
@@ -12,6 +12,12 @@ public class MixinNeoForgeComposter {
     @Inject(method = "getValue", at = @At("RETURN"), cancellable = true, remap = false, require = 0)
     private static void overrideComposterValue(ItemStack stack, CallbackInfoReturnable<Float> cir) {
         if (cir.getReturnValue() > 0.0f) {
+            // Check if the item has been nullified by c:not_compostable tag
+            com.fdimo.compostercompat.CommonClass.CompostData data = com.fdimo.compostercompat.CommonClass.COMPOSTER_CACHE.get(stack.getItem());
+            if (data != null && data.reason != null && data.reason.equals("Tag: c:not_compostable")) {
+                cir.setReturnValue(0.0f);
+                return;
+            }
             if (com.fdimo.compostercompat.CommonClass.DEBUG) {
                 com.fdimo.compostercompat.CommonClass.debugLog("NeoForge Composter recognized item {} natively with chance {}", stack.getHoverName().getString(), cir.getReturnValue());
             }
